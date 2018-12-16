6effe1b5c364243470d45e94c05adca5475eea17
fklinck <francisklinck@gmail.com>
2018-12-16 23:35:21 +0100

    added play & pause button + removerd standard controls + added now playing status + updated style

--- make.sh
+++ make.sh
@@ -17,26 +17,33 @@ function makehtml() {
 	do
 		echo -e "\t<a href=\"$d\">$d</a><br>" >> index.html
 	done
+	
 	#add all songs to the index.html
 	echo "<table>" >> index.html #make a table
 	count=1
 	for file in $files
 	do
-		echo -e "\t<tr>\n\t\t<td>$file</td>\n\t\t<td><audio id=\"$count\" onended=\"playNext(this)\"controls><source src=\"$file\" type=\"audio/mpeg\">No Audio For You</audio></td>\n\t</tr>" >> index.html
+		echo -e "\t<tr>\n\t\t<td>$file</td>\n\t\t<td><button id=\"${count}a\" type=\"button\" onclick=\"play(this)\">Play</button></td><td><button id=\"${count}b\" type=\"button\" onclick=\"stop(this)\">Pause</button><audio id=\"$count\" onended=\"playNext(this)\"><source src=\"$file\" type=\"audio/mpeg\">No Audio For You</audio></td>\n\t</tr>" >> index.html
 
 		count=$((count + 1))
 	done
-	echo "</table>" >> index.html
+	echo "</table>" >> index.html #close the table
 
-	echo "</body>" >> index.html #close the table
+	echo "<h2 id=\"playing\">Now playing: none</h2>" >> index.html
+	
+	echo "</body>" >> index.html 
 	
 	if [[ $count -gt 1 ]]
 	then
-		echo -e "<script>\nfunction playNext(element) {\n\tnewid = parseInt(element.id) + 1;\n\tif (newid > $count) {\n\t\tnewid = 1;\n\t}\n\tdocument.getElementById(newid).play();\n}\n</script>" >> index.html
+		echo -e "<script>" >> index.html
+		echo -e "function playNext(element) {\n\tnewid = parseInt(element.id) + 1;\n\tif (newid > $count) {\n\t\tnewid = 1;\n\t}\n\tdocument.getElementById(newid).play();\n}" >> index.html
+		echo -e "function play(element) {\n\tlet id = element.id.substring(0,1);\ndocument.getElementById(id).play();\ndocument.getElementById(\"playing\").innerHTML = \"Now playing: \" + id;\n}" >> index.html
+		echo -e "function stop(element) {\n\tlet id = element.id.substring(0,1);\ndocument.getElementById(id).pause();\ndocument.getElementById(\"playing\").innerHTML = \"Now playing: none\";\n}" >> index.html
+		echo -e "</script>" >> index.html
 	fi
 
 	echo "</html>" >> index.html
-	echo -e "\e[92mSUCCES\e[0m\t$1"
+	echo -e "\e[92m\e[1mSUCCES\e[0m\t$1"
 }
 
 for dir in $subdirs
